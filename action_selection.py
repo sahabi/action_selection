21ba4ebb105ce8a34960fbd057966efa257141dd
sahabi <sahabi@gmail.com>
2016-07-13 17:57:45 -0500

    new

--- action_selection.py
+++ action_selection.py
@@ -1,39 +1,132 @@
 import operator
 
-def init_probs(P):
+def init_probs(x_size, y_size):
+    P = {}
+    for i in range(0, x_size):
+        for j in range(0, y_size):
+            P[(i, j)] = 0
     num_states = len(P)
     for key in P:
         P[key] = 1./num_states
     return P
 
-def det_k(P,prev_state,action):
+def init_k(x_size, y_size):
+    k = {}
+    for i in range(0, x_size):
+        for j in range(0, y_size):
+            k[(i, j)] = []
+    return k
+
+def det_k(P,prev_state,action, response):
     kl = 1
     kh = 2
     
-    if action == 'east':
+    if action == 'east' and response == 1:
         for key in P:
             if key[0] <= prev_state[0]:
                 P[key] = kl
             if key[0] > prev_state[0]:
                 P[key] = kh
-    if action == 'west':
+    elif action == 'west' and response == 1:
         for key in P:
             if key[0] < prev_state[0]:
                 P[key] = kh
             if key[0] >= prev_state[0]:
                 P[key] = kl
-    if action == 'north':
+    elif action == 'north' and response == 1:
         for key in P:
             if key[1] < prev_state[1]:
                 P[key] = kh
-            if key[0] >= prev_state[0]:
+            if key[1] >= prev_state[0]:
                 P[key] = kl
-    if action == 'south':
+    elif action == 'south' and response == 1:
         for key in P:
             if key[1] <= prev_state[1]:
                 P[key] = kl
             if key[1] < prev_state[1]:
                 P[key] = kh
+
+    elif action == 'east' and response == 0:
+        for key in P:
+            if key[0] <= prev_state[0]:
+                P[key] = kh
+            if key[0] > prev_state[0]:
+                P[key] = kl
+    elif action == 'west' and response == 0:
+        for key in P:
+            if key[0] < prev_state[0]:
+                P[key] = kl
+            if key[0] >= prev_state[0]:
+                P[key] = kh
+    elif action == 'north' and response == 0:
+        for key in P:
+            if key[1] < prev_state[1]:
+                P[key] = kl
+            if key[1] >= prev_state[0]:
+                P[key] = kh
+    elif action == 'south' and response == 0:
+        for key in P:
+            if key[1] <= prev_state[1]:
+                P[key] = kh
+            if key[1] < prev_state[1]:
+                P[key] = kl
+    return P
+
+def det_all_k(s,states,actions, responses):
+    kl = 1
+    kh = 2
+    P = s
+
+    for i in range(len(states)):
+        if actions[i] == 'east' and responses[i] == 1:
+            for key in P:
+                if key[0] <= states[i][0]:
+                    P[key].append(kl)
+                elif key[0] > states[i][0]:
+                    P[key].append(kh)
+        elif actions[i] == 'west' and responses[i] == 1:
+            for key in P:
+                if key[0] < states[i][0]:
+                    P[key].append(kh)
+                elif key[0] >= states[i][0]:
+                    P[key].append(kl)
+        elif actions[i] == 'north' and responses[i] == 1:
+            for key in P:
+                if key[1] < states[i][1]:
+                    P[key].append(kh)
+                elif key[1] >= states[i][1]:
+                    P[key].append(kl)
+        elif actions[i] == 'south' and responses[i] == 1:
+            for key in P:
+                if key[1] <= states[i][1]:
+                    P[key].append(kl)
+                elif key[1] > states[i][1]:
+                    P[key].append(kh)
+
+        elif actions[i] == 'east' and responses[i] == 0:
+            for key in P:
+                if key[0] <= states[i][0]:
+                    P[key].append(kh)
+                elif key[0] > states[i][0]:
+                    P[key].append(kl)
+        elif actions[i] == 'west' and responses[i] == 0:
+            for key in P:
+                if key[0] < states[i][0]:
+                    P[key].append(kl)
+                elif key[0] >= states[i][0]:
+                    P[key].append(kh)
+        elif actions[i] == 'north' and responses[i] == 0:
+            for key in P:
+                if key[1] < states[i][1]:
+                    P[key].append(kl)
+                elif key[1] >= states[i][1]:
+                    P[key].append(kh)
+        elif actions[i] == 'south' and responses[i] == 0:
+            for key in P:
+                if key[1] <= states[i][1]:
+                    P[key].append(kh)
+                elif key[1] > states[i][1]:
+                    P[key].append(kl)
     return P
 
 def update_P(P,K):
@@ -44,84 +137,108 @@ def update_P(P,K):
         P[key] = K[key]*P[key]/float(norm)
     return P
 
+def update_all_P(P,K):
+    #print K
+    for i in range(len(K[(0,0)])):
+        norm = 0
+        for key in P:
+            norm += P[key]*K[key][i]
+        for key in P:
+            P[key] = K[key][i]*P[key]/float(norm)
+    return P
+
 def get_max(P):
     maximum =  max(P.iteritems(), key=operator.itemgetter(1))[1]
-    max_s = []
+    maxim = []
     for key in P:
         if P[key] == maximum:
-            max_s.append(key)
-    return max_s
+            maxim.append(key)
+    return maxim
 
-def get_min_states(P):
-    maximum =  min(P.iteritems(), key=operator.itemgetter(1))[1]
+def get_min(P):
+    minimum =  min(P.iteritems(), key=operator.itemgetter(1))[1]
     min_s = []
     for key in P:
-        if P[key] == maximum:
+        if P[key] == minimum:
             min_s.append(key)
-    if len(min_s) == 1:
-        return min_s
-    else:
-        return min_s[0]
+    return min_s[0]
 
-def manhattan_dist(s,st):
-    return abs(s[0] - st[0]) + abs(s[1] - st[1])
+def get_min_actions(actions):
+    minimum =  min(actions.iteritems(), key=operator.itemgetter(1))[1]
+    min_a = []
+    for key in actions:
+        if actions[key] == minimum:
+            min_a.append(key)
+    return min_a
 
-def get_target(s,s_max):
+def manhattan_dist(state_a,state_b):
+    return abs(state_a[0] - state_b[0]) + abs(state_a[1] - state_b[1])
+
+def get_target(current_state,S_max):
     state_dict = {}
-    for state in s_max:
-        if state != s:
-            state_dict[state] = manhattan_dist(s,state)
-    return get_min_states(state_dict)
+    for state in S_max:
+        if state != current_state:
+            state_dict[state] = manhattan_dist(current_state, state)
+    return get_min(state_dict)
 
-def move(s,a):
-    if a == 'east':
+def move(s, a, x_size, y_size): 
+    if a == 'east' and s[0] != x_size - 1:
         return (s[0]+1,s[1])
-    if a == 'north':
+    if a == 'north' and s[1] != 0:
         return (s[0],s[1]-1)
-    if a == 'west':
+    if a == 'west' and s[0] != 0:
         return (s[0]-1,s[1])
-    if a == 'south':
+    if a == 'south' and s[1] != y_size - 1:
         return (s[0],s[1]+1)
+    else:
+        return s
         
-def get_s_h_l(current_s, action, S_max):
-    
+def get_s_h_l(current_s, action, S_max, x_size, y_size):   
     s_h = {}
+    print S_max
     s_h[(current_s,action)] = []
     s_l = {}
     s_l[(current_s,action)] = []
     for state in S_max:
-        if manhattan_dist(move(current_s,action),state) <= manhattan_dist(current_s,state):
+        if manhattan_dist(move(current_s,action, x_size, y_size),state) <= manhattan_dist(current_s,state):
             s_h[(current_s,action)].append(state)
-        if manhattan_dist(move(current_s,action),state) > manhattan_dist(current_s,state):
+        if manhattan_dist(move(current_s,action, x_size, y_size),state) > manhattan_dist(current_s,state):
             s_l[(current_s,action)].append(state)
     return(s_h,s_l)
 
-def get_delta_action(current_s, S_max, actions):
+def get_delta_action(current_s, S_max, actions, x_size, y_size):
     delta_action_dict = {}
-    
     for action in actions:
-        (s_h,s_l) = get_s_h_l(current_s, S_max, action)
+        (s_h,s_l) = get_s_h_l(current_s, action, S_max, x_size, y_size)
         s_h_num = len(s_h[(current_s,action)])
         s_l_num = len(s_l[(current_s,action)])
         delta_action = abs(s_h_num - s_l_num)
         delta_action_dict[action] = delta_action
-        
+    print delta_action_dict
     return delta_action_dict
     
-def pick_action(delta_action_dict, current_state, target_state):
-    actions = get_max(delta_action_dict)
+def pick_action(current_state, actions, P, x_size, y_size):
+    print P
+    S_max = get_max(P)
+
+    target_state = get_target(current_state, S_max)
+    delta_action_dict = get_delta_action(current_state, S_max, actions, x_size, y_size)
+    actions = get_min_actions(delta_action_dict)
     if len(actions) == 1:
         return actions[0]
     if len(actions) > 1:
         for action in actions:
-            if manhattan_dist(move(current_state,action),target_state) < manhattan_dist(current_state,target_state):
+            if manhattan_dist(move(current_state,action, x_size, y_size),target_state) < manhattan_dist(current_state,target_state):
                 return action
 
-
-P = {(0,0):0,(0,1):0,(0,2):0,(0,3):0,(1,0):0,(1,1):0,(1,2):0,(1,3):0,(2,0):0,(2,1):0,(2,2):0,(2,3):0,(3,0):0,(3,1):0,(3,2):0,(3,3):0}
-P = init_probs(P)
-K = det_k(P,(0,3),'east')
-P = update_P(P,K)
-S = get_max(P)
-T = get_target((2,0),S)
-print T
+if __name__=="__main__":
+    x_size = 4
+    y_size = 4
+    P = init_probs(x_size, y_size)
+    K = det_k(P,(0,3),'east')
+    P = update_P(P,K)
+    S = get_max(P)
+    T = get_target((2,0),S)
+    action = pick_action((0, 3), ['east','west','north','south'], P, x_size, y_size)
+    print T
+    print action
